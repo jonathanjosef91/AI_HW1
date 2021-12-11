1554c7ffef0ab7d083b136a537b0289cd83fe91d
tayaharshuk <57790114+tayaharshuk@users.noreply.github.com>
2021-12-11 10:16:01 +0200

    c

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ from Experiments import *
 
 
 if __name__ == "__main__":
-    test_robot(UniformCostSearchRobot, [99])
-    test_robot(WAStartRobot, [99], heuristic=tail_manhattan_heuristic)
-    #  test_robot(WAStartRobot, [0, 1, 2, 3, 4, 5], heuristic=center_manhattan_heuristic)
+    for k in [2,4,6,8]:
+        test_robot(WAStartRobot,[3,4],heuristic=ShorterRobotHeuristic, k=k)
+    #test_robot(UniformCostSearchRobot, [99])
+    #test_robot(WAStartRobot, [99], heuristic=tail_manhattan_heuristic)
+    # test_robot(WAStartRobot, [0, 1, 2, 3, 4, 5], heuristic=center_manhattan_heuristic)
     # solve_and_display(WAStartRobot, 2, blit=False, heuristic=tail_manhattan_heuristic)
     # w_experiment(2)
 
